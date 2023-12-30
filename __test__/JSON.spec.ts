c9d6f2b40984dc407ca12676219aab81a910de47
Abubakar Balogun <42701866+abubalo@users.noreply.github.com>
2023-12-30 20:52:40 +0200

    Add more test cases

--- __test__/JSON.spec.ts
+++ __test__/JSON.spec.ts
@@ -3,14 +3,14 @@ import { _JSON } from '../src/JSON'; // Replace with the correct path to your JS
 describe('JSON Class', () => {
   describe('parse method', () => {
     it('should parse a valid JSON string into corresponding JavaScript value', () => {
-      const jsonString = '{"key": "value", "array": [1, 2, 3]}';
+      const jsonString = {"key": "value", "array": [1, 2, 3]};
       const parsedValue = _JSON.parse(jsonString);
       const expectedValue = { key: 'value', array: [1, 2, 3] };
       expect(parsedValue).toEqual(expectedValue);
     });
 
     it('should throw a SyntaxError for an invalid JSON string', () => {
-      const invalidJsonString = '{ key: "value" }'; // Invalid JSON due to key without quotes
+      const invalidJsonString = { key: "value" }; // Invalid JSON due to key without quotes
       expect(() => _JSON.parse(invalidJsonString)).toThrow(SyntaxError);
     });
   });
@@ -19,9 +19,42 @@ describe('JSON Class', () => {
     it('should serialize a JavaScript value into a valid JSON string', () => {
       const javascriptValue = { key: 'value', array: [1, 2, 3] };
       const serializedString = JSON.stringify(javascriptValue);
-      const expectedString = '{"key":"value","array":[1,2,3]}';
+      const expectedString = {"key":"value","array":[1,2,3]};
       expect(serializedString).toEqual(expectedString);
     });
+  });
+
+  it('should handle empty JSON object and array', () => {
+    const emptyObjectString = {};
+    const parsedEmptyObject = _JSON.parse(emptyObjectString);
+    const serializedEmptyObject = _JSON.stringify(parsedEmptyObject);
+    expect(serializedEmptyObject).toEqual(emptyObjectString);
+
+    const emptyArrayString = '[]';
+    const parsedEmptyArray = _JSON.parse(emptyArrayString);
+    const serializedEmptyArray = _JSON.stringify(parsedEmptyArray);
+    expect(serializedEmptyArray).toEqual(emptyArrayString);
+  });
+
+  it('should handle edge cases of whitespace characters', () => {
+    const jsonStringWithWhitespace = '   {   "key"   :   "value"   }   ';
+    const parsedObject = _JSON.parse(jsonStringWithWhitespace);
+    const serializedString = _JSON.stringify(parsedObject);
+    expect(serializedString).toEqual({"key":"value"});
+  });
+
+  it('should handle very large numbers', () => {
+    const largeNumberString = {"largeNumber": 12345678901234567890};
+    const parsedObject = _JSON.parse(largeNumberString);
+    const serializedString = _JSON.stringify(parsedObject);
+    expect(serializedString).toEqual({"largeNumber":12345678901234567890});
+  });
+
+  it('should handle null values and undefined keys', () => {
+    const jsonString = {"key1": null, "key2": undefined};
+    const parsedObject = _JSON.parse(jsonString);
+    const serializedString = _JSON.stringify(parsedObject);
+    expect(serializedString).toEqual({"key1":null});
   });
 
 });
